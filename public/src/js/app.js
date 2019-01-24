48912cb1af410d62f7f833b41eb7f8f8555b9881
Andrew <andrewpeliza@hotmail.com>
2019-01-24 18:29:24 +0000

    Promise and Fetching next will be working with Cache.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,10 +1,17 @@
 let deferredPrompt;
 
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-            .then( () => {
+            .then(() => {
                 console.log('Service Worker Registerd!');
+            })
+            .catch((err) => {
+                console.log(err);
             });
 }
 
@@ -14,3 +21,44 @@ window.addEventListener('beforeinstallprompt', (event) => {
     deferredPrompt = event;
     return false;
 });
+
+
+fetch('https://httpbin.org/ip')
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Does this work?'})
+})
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+// let promise = new Promise((resolve, reject) => {
+//     setTimeout()
+// });
+
+// promise.then((text) => {
+
+// })
